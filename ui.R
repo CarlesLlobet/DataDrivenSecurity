e05688666859dff6937f0fd350d7975aa566a6d7
master6_cs10 <master6_cs10@a5s104mac54.fib.upc.es>
2017-12-19 20:01:36 +0100

    afegit projecte amb mapa

---
+++ ui.R
@@ -0,0 +1,69 @@
+library(leaflet)
+
+# Choices for drop-downs
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
+
+
+navbarPage("Fortinet connections analyzer", id="nav",
+
+  tabPanel("Interactive map",
+    div(class="outer",
+
+      tags$head(
+        # Include our custom CSS
+        includeCSS("styles.css"),
+        includeScript("gomap.js")
+      ),
+
+      # If not using custom CSS, set height of leafletOutput to a number instead of percent
+      leafletOutput("map", width="100%", height="100%"),
+
+      # Shiny versions prior to 0.11 should use class = "modal" instead.
+      absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+        draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+        width = 330, height = "auto",
+
+        h2("Filter connections"),
+
+        selectInput("color", "Color", vars),
+        selectInput("size", "Size", vars, selected = "adultpop")
+      )
+    )
+  ),
+
+  tabPanel("Data explorer",
+    fluidRow(
+      column(3,
+        selectInput("states", "States", c("All states"="", structure(state.abb, names=state.name), "Washington, DC"="DC"), multiple=TRUE)
+      ),
+      column(3,
+        conditionalPanel("input.states",
+          selectInput("cities", "Cities", c("All cities"=""), multiple=TRUE)
+        )
+      ),
+      column(3,
+        conditionalPanel("input.states",
+          selectInput("zipcodes", "Zipcodes", c("All zipcodes"=""), multiple=TRUE)
+        )
+      )
+    ),
+    fluidRow(
+      column(1,
+        numericInput("minScore", "Min score", min=0, max=100, value=0)
+      ),
+      column(1,
+        numericInput("maxScore", "Max score", min=0, max=100, value=100)
+      )
+    ),
+    hr(),
+    DT::dataTableOutput("ziptable")
+  ),
+
+  conditionalPanel("false", icon("crosshair"))
+)
